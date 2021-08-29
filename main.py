e663edb2cf5ce304190e57606fccb98e142d60d4
Chirag Mandaviya <88018551+chiraghm12@users.noreply.github.com>
2021-08-29 14:03:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import cv2
 import matplotlib.pyplot as plt
 
-img=cv2.imread("123.jpg")
+img=cv2.imread("aston_martin_dbc_concept_2013-1366x768.jpg")
 cv2.imshow("original image",img)
 cv2.waitKey(0)
 
